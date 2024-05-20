611f9f72db8cdad97dd266bb916b7f2698f7ab35
EvaGupta02 <150214201+EvaGupta02@users.noreply.github.com>
2024-05-20 12:27:16 +0530

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,16 @@
+const container = document.querySelector(".container"),
+signUp = document.querySelector(".signup-link"),
+login = document.querySelector(".login-link");
+
+// js code to appear signup and login form
+signUp.addEventListener("click", ( )=>{
+  container.classList.add("active");
+});
+login.addEventListener("click", ( )=>{
+  container.classList.remove("active");
+});
+
+
+
+
+
